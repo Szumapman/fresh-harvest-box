5dee70ae2a040daf8d1985cb0d60c4b066e1a3e4
Paweł Szumański <pawel.szumanski@outlook.com>
2025-06-14 01:32:00 +0200

    create init() and add hamburger menu

--- src/main.js
+++ src/main.js
@@ -1,1 +1,17 @@
+import 'modern-normalize/modern-normalize.css';
+import './styles/main.scss';
 
+const init = () => {
+    const hamburgerButton = document.querySelector('#hamburger-menu');
+    const burgerIcon = document.querySelector('#burger');
+    const closeIcon = document.querySelector('#close');
+    const mobileNavList = document.querySelector('.nav-mobile-list');
+
+    hamburgerButton.addEventListener('click', () => {
+        mobileNavList.classList.toggle('active');
+        closeIcon.classList.toggle('active');
+        burgerIcon.classList.toggle('active');
+    });
+}
+
+document.addEventListener('DOMContentLoaded', init);
