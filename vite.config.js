c3eb6a0c37b874e6c2800c89e778ecc39a4a5d54
Paweł Szumański <pawel.szumanski@outlook.com>
2025-07-12 17:12:50 +0200

    add posthtml include

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,35 @@
 import { defineConfig } from 'vite';
 import path from 'path';
 import posthtml from '@vituum/vite-plugin-posthtml';
+import posthtmlInclude from 'posthtml-include';
 
 export default defineConfig({
-  plugins: [posthtml()],
+  root: '.',
+
+  build: {
+    outDir: 'dist',
+    emptyOutDir: true,
+    rollupOptions: {
+      input: 'index.html'
+    }
+  },
+
+  plugins: [
+    posthtml({
+      plugins: [
+        posthtmlInclude({
+          root: path.resolve(__dirname, 'src/partials')
+        })
+      ]
+    })
+  ],
+
   css: {
     preprocessorOptions: {
       scss: {
         additionalData: `@use "variables" as *;`,
         sassOptions: {
-          includePaths: [
-            path.resolve(__dirname, 'src/styles')
-          ]
+          includePaths: [path.resolve(__dirname, 'src/styles')]
         }
       }
     }
