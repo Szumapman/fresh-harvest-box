0103abf18fd2489f195d36f57a4c660d6b40e8be
Paweł Szumański <pawel.szumanski@outlook.com>
2025-07-12 18:32:40 +0200

    refactor

--- vite.config.js
+++ vite.config.js
@@ -4,34 +4,35 @@ import posthtml from '@vituum/vite-plugin-posthtml';
 import posthtmlInclude from 'posthtml-include';
 
 export default defineConfig({
-  root: '.',
+    base: './',
+    root: '.',
 
-  build: {
-    outDir: 'dist',
-    emptyOutDir: true,
-    rollupOptions: {
-      input: 'index.html'
-    }
-  },
-
-  plugins: [
-    posthtml({
-      plugins: [
-        posthtmlInclude({
-          root: path.resolve(__dirname, 'src/partials')
-        })
-      ]
-    })
-  ],
-
-  css: {
-    preprocessorOptions: {
-      scss: {
-        additionalData: `@use "variables" as *;`,
-        sassOptions: {
-          includePaths: [path.resolve(__dirname, 'src/styles')]
+    build: {
+        outDir: 'dist',
+        emptyOutDir: true,
+        rollupOptions: {
+            input: 'index.html'
         }
-      }
+    },
+
+    plugins: [
+        posthtml({
+            plugins: [
+                posthtmlInclude({
+                    root: path.resolve(__dirname, 'src/partials')
+                })
+            ]   
+        })
+    ],
+
+    css: {
+        preprocessorOptions: {
+            scss: {
+                additionalData: `@use "variables" as *;`,
+                sassOptions: {
+                    includePaths: [path.resolve(__dirname, 'src/styles')]
+                }
+            }
+        }
     }
-  }
 });
